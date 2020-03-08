9d84250f78bca12001dc5d8973fdeee20e8a4d66
Otávio Bonatto <31021991+OtavioBonatto@users.noreply.github.com>
2020-03-08 17:57:20 -0300

    Leaves

--- 2d endless runner test/Assets/Scripts/LeavesGenerator.cs
+++ 2d endless runner test/Assets/Scripts/LeavesGenerator.cs
@@ -9,7 +9,6 @@ public class LeavesGenerator : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-        leaf.
     }
 
     // Update is called once per frame
